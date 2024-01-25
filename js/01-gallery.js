ab76d6f6b18e770a3cf73090a1210b7fed206b7b
EnChyb <144510772+EnChyb@users.noreply.github.com>
2024-01-25 22:54:08 +0100

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divSelector = document.querySelector(".gallery");
+
+const divGallery = (items) => {
+    return items
+        .map(item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`)
+        .join("");
+    
+}
+
+const makeGallery = divGallery(galleryItems);
+
+divSelector.innerHTML = makeGallery;
+
+divSelector.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") { 
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+    instance.show();
+
+    divSelector.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    })
+
+
+
+})
